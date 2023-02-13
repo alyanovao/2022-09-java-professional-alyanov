ee09ea4cf289dde4412cf63e8a113750f2c6ecfa
Альянов Артем Олегович <domovoy175@gmail.com>
2023-02-13 21:56:19 +0300

    Поправил скрипт миграции

--- homework-14/src/main/resources/db/migration/V2__create client.sql
+++ homework-14/src/main/resources/db/migration/V2__create client.sql
@@ -1,5 +1,5 @@
 -- Для @GeneratedValue(strategy = GenerationType.SEQUENCE)
-insert into address (street) values ('Krasnaya');
+insert into address (street, client_id) values ('Krasnaya', 1);
 insert into phone (number, client_id) values ('89181111111', 1);
 insert into phone (number, client_id) values ('89181111118', 1);
 insert into client (name, address_id, phone_id) values ('Иванов', 1, 1);
